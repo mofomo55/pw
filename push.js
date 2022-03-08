2052bc970df93ad224441bd70c2af73d8d524ec7
Mohamad Eibo <Mohamad@intern.hollaus.at>
2022-03-08 12:09:48 +0100

    fix

--- push.js
+++ push.js
@@ -7,9 +7,9 @@ let vapidKey = {
 
 
 
-push.setVapidDetails('wwrf3f3r333@gmail.com', vapidKey.publicKey, vapidKey.privateKey)
+push.setVapidDetails('wwrf3f3r33', vapidKey.publicKey, vapidKey.privateKey)
 
 
-let sub={};
+let sub={"endpoint":"https://fcm.googleapis.com/fcm/send/cd2ZlCTOSH8:APA91bGV6IRK20pUWF6BtXleVPV6xFJa--sbq6Ab44ZiL419qhXZ-3pAdqz1XtuYvdUNPxNI0S3bwPDrn8Mq_QSPjhUsFPwKmiZGyp_V5KdEMi5hd0ra8PiYGyFnO00cLkKpzkm51cdE","expirationTime":null,"keys":{"p256dh":"BNdBvASEbmbQzU8Yda8ahn894FUjJPG1GAg-JoTwa4GyLNUqtCZoFUyT-dq81tgYcDG7YTra1vGG4oT7kzVyMfg","auth":"XsAueXDbDKBMEb8Ka9r8Wg"}};
 
 push.sendNotification(sub, 'test message')
